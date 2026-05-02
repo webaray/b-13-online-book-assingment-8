6fb90449a7b23c57d811929b3647a33d6901605e
webaray <josimuddin105464@gmail.com>
2026-05-02 18:38:30 +0600

    Setup better auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,26 +1,47 @@
-import { betterAuth } from 'better-auth';
-import { mongodbAdapter } from 'better-auth/adapters/mongodb';
-import { getDb } from './db';
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("Bookonline");
 
 export const auth = betterAuth({
-  database: mongodbAdapter(await getDb()),
-  secret: process.env.BETTER_AUTH_SECRET,
-  baseURL: process.env.BETTER_AUTH_URL,
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
   emailAndPassword: {
     enabled: true
   },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
-    }
-  },
-  user: {
-    changeEmail: {
-      enabled: false
-    },
-    deleteUser: {
-      enabled: false
-    }
-  }
 });
+
+
+
+
+
+// import { betterAuth } from 'better-auth';
+// import { mongodbAdapter } from 'better-auth/adapters/mongodb';
+// import { getDb } from './db';
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(await getDb()),
+//   secret: process.env.BETTER_AUTH_SECRET,
+//   baseURL: process.env.BETTER_AUTH_URL,
+//   emailAndPassword: {
+//     enabled: true
+//   },
+//   socialProviders: {
+//     google: {
+//       clientId: process.env.GOOGLE_CLIENT_ID || '',
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+//     }
+//   },
+//   user: {
+//     changeEmail: {
+//       enabled: false
+//     },
+//     deleteUser: {
+//       enabled: false
+//     }
+//   }
+// });
